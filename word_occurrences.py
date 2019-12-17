afceebb68af5cf2a68e64f074ba7c87ae6cf0545
KhantKyawZin <khantkyawzin@my.jcu.edu.au>
2019-12-18 01:45:32 +0800

    Initial commit

---
+++ word_occurrences.py
@@ -0,0 +1,13 @@
+count_word= {}
+input_text = input("Text: ")
+words = input_text.split()
+for word in words:
+    i = count_word.get(word, 0)
+    count_word[word] = i + 1
+
+words = list(count_word.keys())
+words.sort()
+max_length = max((len(word) for word in words))
+
+for word in words:
+    print("{:{}} : {}".format(word, max_length, count_word[word]))
